e0db8ec6ce91186932c1fcbb9b1c78aa84426e36
xupeng <xvpeng9060@sina.com>
2018-08-22 08:55:54 +0800

    Resnet demo

---
+++ ResNets/dataset_info.py
@@ -0,0 +1,28 @@
+from resnets_utils import *
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+
+def info(orig_data):
+
+    X_train, Y_train, X_test, Y_test = process_orig_datasets(orig_data)
+
+    print ("number of training examples = " + str(X_train.shape[0]))
+    print ("number of test examples = " + str(X_test.shape[0]))
+    print ("X_train shape: " + str(X_train.shape))
+    print ("Y_train shape: " + str(Y_train.shape))
+    print ("X_test shape: " + str(X_test.shape))
+    print ("Y_test shape: " + str(Y_test.shape))
+
+    print('x train max, ', np.max(X_train), '; x train min, ', np.min(X_train))
+    print('x test max, ', np.max(X_test), '; x test min, ', np.min(X_test))
+
+    mini_batch = random_mini_batches(X_train, Y_train)
+    print(mini_batch[0][0].shape)
+
+    # mini_batches = random_mini_batches(X_train, Y_train, mini_batch_size=16)
+    #
+    # X_mini_batch, Y_mini_batch = mini_batches[np.random.randint(0, len(mini_batches))]
+
+
